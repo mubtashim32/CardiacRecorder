1051ed1e9f5b268c7c797ef100f46e5af385edff
Nihal <mubtashim32@gmail.com>
2023-07-04 16:26:59 +0600

    Measurement Class Updated

--- app/src/main/java/com/example/cardiacrecorder/HomeActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/HomeActivity.java
@@ -1,14 +1,31 @@
 package com.example.cardiacrecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ArrayList<CardiacMeasurement> cardiacMeasurementArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView cardiacMeasurements = findViewById(R.id.cardiacMeasurements);
+
+//        CardiacMeasurement cardiacMeasurement = new CardiacMeasurement(1, 2, 3, "Hello");
+        cardiacMeasurementArrayList.add(cardiacMeasurement);
+
+        CardiacMeasurementsAdapter cardiacMeasurementsAdapter = new CardiacMeasurementsAdapter(cardiacMeasurementArrayList);
+        cardiacMeasurements.setAdapter(cardiacMeasurementsAdapter);
+        cardiacMeasurements.setLayoutManager(new LinearLayoutManager(this));
     }
 }
